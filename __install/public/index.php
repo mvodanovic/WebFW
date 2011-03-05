03a85b5b2d620fcff040950c78a75412f25991a7
Marko Vodanović <marko.vodanovic@yahoo.com>
2011-03-05 23:37:00 +0100

    index.php

---
+++ __install/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!file_exists('../webfw/config/bootstrap.inc.php'))
+{
+   echo 'WebFW: FATAL ERROR: Bootstrap function unreachable: ' . realpath('../webfw/config/bootstrap.inc.php');
+   die;
+}
+
+require_once('../webfw/config/bootstrap.inc.php');
+
+\WebFW\Config\Bootstrap();
+
+try
+{
+   \WebFW\Core\Framework::Start();
+}
+catch (\WebFW\Core\Exception $e)
+{
+   $e->ErrorMessage();
+   die;
+}
+
+?>
