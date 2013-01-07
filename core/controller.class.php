889cec8c79fba11c6bd7b57e50e92ba852076adb
Marko Vodanovic <marko@vodanovic.net>
2013-01-07 22:01:39 +0100

    controller update - old code cleanup

--- core/controller.class.php
+++ core/controller.class.php
@@ -10,32 +10,24 @@ use \ReflectionMethod;
 abstract class Controller
 {
     protected $template = 'default';
-    protected $baseTemplate = 'default';
-    protected $pageTitle = '';
     protected $useTemplate = true;
-    protected $simpleOutput = false;
     protected $redirectUrl = null;
     protected $templateVariables = array();
-
-    protected $_action;
-    protected $_className;
-    private $_urlJS = array();
-    private $_urlCSS = array();
-    private $_htmlMeta = array();
-    private $_customHtmlHead = '';
+    protected $action;
+    protected $className;
 
     const DEFAULT_ACTION_NAME = 'execute';
 
     public function __construct()
     {
-        $this->_action = $this->getDefaultActionName();
+        $this->action = $this->getDefaultActionName();
 
         $value = '';
         if (array_key_exists('action', $_REQUEST)) {
             $value = trim($_REQUEST['action']);
         }
         if ($value !== null && $value !== '') {
-            $this->_action = $value;
+            $this->action = $value;
         }
 
         $value = Data::GetItem('DEFAULT_CTL_TEMPLATE');
@@ -43,37 +35,27 @@ abstract class Controller
             $this->template = $value;
         }
 
-        $value = Data::GetItem('DEFAULT_BASE_TEMPLATE');
-        if ($value !== null) {
-            $this->baseTemplate = $value;
-        }
-
-        $this->_className = get_class($this);
+        $this->className = get_class($this);
     }
 
-    final public function GetTitle()
+    public function init()
     {
-        return $this->pageTitle;
-    }
-
-    final public function Init()
-    {
-        $action = $this->_action;
+        $action = $this->action;
 
         if (!method_exists($this, $action)) {
-            $this->error404('Action not defined: ' . $action . ' (in controller ' . $this->_className . ')');
+            $this->error404('Action not defined: ' . $action . ' (in controller ' . $this->className . ')');
         }
 
         $reflection = new ReflectionMethod($this, $action);
         if (!$reflection->isPublic()) {
-            $this->error404('Action not declared as public: ' . $action . ' (in controller ' . $this->_className . ')');
+            $this->error404('Action not declared as public: ' . $action . ' (in controller ' . $this->className . ')');
         }
 
         if ($reflection->isStatic()) {
-            $this->error404('Action declared as static: ' . $action . ' (in controller ' . $this->_className . ')');
+            $this->error404('Action declared as static: ' . $action . ' (in controller ' . $this->className . ')');
         }
 
-        if ($this->_action !== $this->getDefaultActionName()) {
+        if ($this->action !== $this->getDefaultActionName()) {
             $this->template = strtolower($action);
         }
 
@@ -87,7 +69,7 @@ abstract class Controller
             return;
         }
 
-        $templateDir = explode('\\', $this->_className);
+        $templateDir = explode('\\', $this->className);
         $templateDir = strtolower(end($templateDir));
         $templateDir = \WebFW\Config\CTL_TEMPLATE_PATH . DIRECTORY_SEPARATOR . $templateDir . DIRECTORY_SEPARATOR;
 
@@ -100,93 +82,7 @@ abstract class Controller
             $template->set($name, $value);
         }
 
-        $wFW_HtmlBody = $template->fetch();
-        $wFW_HtmlHead = $this->_customHtmlHead;
-
-        foreach ($this->_urlJS as &$url) {
-            $wFW_HtmlHead .= '<script type="text/javascript" src="' . $url . '"></script>' . "\n";
-        }
-
-        foreach ($this->_urlCSS as &$data) {
-            switch ($data['xhtml']) {
-                case true:
-                    $wFW_HtmlHead .= '<link rel="stylesheet" type="text/css" href="' . $data['url'] . '" />' . "\n";
-                    break;
-                default:
-                    $wFW_HtmlHead .= '<link rel="stylesheet" type="text/css" href="' . $data['url'] . '">' . "\n";
-                    break;
-            }
-        }
-
-        /// key, content, keyType=name, scheme='', xhtml=true
-        foreach ($this->_htmlMeta as &$data) {
-            if ($data['keyType'] !== 'name' && $data['keyType'] !== 'http-equiv') {
-                continue;
-            }
-
-            if ($data['scheme'] !== '') {
-                $data['scheme'] = ' scheme="' . $data['scheme'] . '"';
-            }
-
-            switch ($data['xhtml']) {
-                case true:
-                    $wFW_HtmlHead .= '<meta ' . $data['keyType'] . '="' . $data['key'] . '" content="' . $data['content'] . '"' . $data['scheme'] . ' />' . "\n";
-                    break;
-                default:
-                    $wFW_HtmlHead .= '<meta ' . $data['keyType'] . '="' . $data['key'] . '" content="' . $data['content'] . '"' . $data['scheme'] . '>' . "\n";
-                    break;
-            }
-        }
-
-        if ($this->simpleOutput === false) {
-            $templateDir = \WebFW\Config\BASE_TEMPLATE_PATH . DIRECTORY_SEPARATOR;
-
-            try {
-                $template = new PHPTemplate($this->baseTemplate . '.template.php', $templateDir);
-            } catch (Exception $e) {
-                throw new Exception('Base template missing: ' . $templateDir . $this->baseTemplate . '.template.php');
-            }
-            foreach ($this->templateVariables as $name => &$value) {
-                $template->set($name, $value);
-            }
-            $template->set('wFW_HtmlBody', $wFW_HtmlBody);
-            $template->set('wFW_HtmlHead', $wFW_HtmlHead);
-            $template->set('wFW_PageTitle', $this->pageTitle);
-            $template->set('wFW_Controller', $this);
-
-            $wFW_HtmlBody = $this->doctype . $template->fetch();
-        }
-
-        echo $wFW_HtmlBody;
-    }
-
-    final protected function setLinkedJavaScript($url)
-    {
-        $this->_urlJS[] = $url;
-    }
-
-    final protected function setLinkedCSS($url, $xhtml = true)
-    {
-        $this->_urlCSS[] = array(
-            'url'    => $url,
-            'xhtml'  => $xhtml === true ? true : false
-        );
-    }
-
-    final protected function setHtmlMeta($key, $content, $keyType = 'name', $scheme = '', $xhtml = true)
-    {
-        $this->_htmlMeta[] = array(
-            'key'       => $key,
-            'content'   => $content,
-            'keyType'   => strtolower($keyType) === 'name' ? 'name' : 'http-equiv',
-            'scheme'    => $scheme,
-            'xhtml'     => $xhtml === true ? true : false
-        );
-    }
-
-    final protected function setCustomHtmlHead($html)
-    {
-        $this->_customHtmlHead = $html;
+        echo $template->fetch();
     }
 
     protected function error404($debugMessage = '404 Not Found')
@@ -194,7 +90,7 @@ abstract class Controller
         Framework::Error404($debugMessage);
     }
 
-    final protected function SetTplVar($name, $value)
+    final protected function setTplVar($name, $value)
     {
         $this->templateVariables[$name] = $value;
     }
---
+++ core/htmlcontroller.class.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace WebFW\Core;
+
+use \WebFW\Externals\PHPTemplate;
+use \WebFW\Core\Framework;
+use \Config\Specifics\Data;
+
+abstract class HTMLController extends Controller
+{
+    protected $baseTemplate = 'default';
+    protected $pageTitle = '';
+    protected $simpleOutput = false;
+    protected $urlJS = array();
+    protected $urlCSS = array();
+    protected $htmlMeta = array();
+    protected $customHtmlHead = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $value = Data::GetItem('DEFAULT_BASE_TEMPLATE');
+        if ($value !== null) {
+            $this->baseTemplate = $value;
+        }
+    }
+
+    public function getTitle()
+    {
+        return $this->pageTitle;
+    }
+
+    public function init()
+    {
+        ob_start();
+        parent::init();
+        $htmlBody = ob_get_clean();
+        $htmlHead = $this->customHtmlHead;
+
+        foreach ($this->urlJS as &$url) {
+            $htmlHead .= '<script type="text/javascript" src="' . $url . '"></script>' . "\n";
+        }
+
+        foreach ($this->urlCSS as &$data) {
+            switch ($data['xhtml']) {
+                case true:
+                    $htmlHead .= '<link rel="stylesheet" type="text/css" href="' . $data['url'] . '" />' . "\n";
+                    break;
+                default:
+                    $htmlHead .= '<link rel="stylesheet" type="text/css" href="' . $data['url'] . '">' . "\n";
+                    break;
+            }
+        }
+
+        /// key, content, keyType=name, scheme='', xhtml=true
+        foreach ($this->htmlMeta as &$data) {
+            if ($data['keyType'] !== 'name' && $data['keyType'] !== 'http-equiv') {
+                continue;
+            }
+
+            if ($data['scheme'] !== '') {
+                $data['scheme'] = ' scheme="' . $data['scheme'] . '"';
+            }
+
+            switch ($data['xhtml']) {
+                case true:
+                    $htmlHead .= '<meta ' . $data['keyType'] . '="' . $data['key'] . '" content="' . $data['content'] . '"' . $data['scheme'] . ' />' . "\n";
+                    break;
+                default:
+                    $htmlHead .= '<meta ' . $data['keyType'] . '="' . $data['key'] . '" content="' . $data['content'] . '"' . $data['scheme'] . '>' . "\n";
+                    break;
+            }
+        }
+
+        if ($this->simpleOutput === false) {
+            $templateDir = \WebFW\Config\BASE_TEMPLATE_PATH . DIRECTORY_SEPARATOR;
+
+            try {
+                $template = new PHPTemplate($this->baseTemplate . '.template.php', $templateDir);
+            } catch (Exception $e) {
+                throw new Exception('Base template missing: ' . $templateDir . $this->baseTemplate . '.template.php');
+            }
+            foreach ($this->templateVariables as $name => &$value) {
+                $template->set($name, $value);
+            }
+            $template->set('htmlBody', $htmlBody);
+            $template->set('htmlHead', $htmlHead);
+            $template->set('pageTitle', $this->pageTitle);
+            $template->set('controller', $this);
+
+            $htmlBody = $template->fetch();
+        }
+
+        echo $htmlBody;
+    }
+
+    protected function setLinkedJavaScript($url)
+    {
+        $this->urlJS[] = $url;
+    }
+
+    protected function setLinkedCSS($url, $xhtml = true)
+    {
+        $this->urlCSS[] = array(
+            'url'    => $url,
+            'xhtml'  => $xhtml === true ? true : false
+        );
+    }
+
+    protected function setHtmlMeta($key, $content, $keyType = 'name', $scheme = '', $xhtml = true)
+    {
+        $this->htmlMeta[] = array(
+            'key'       => $key,
+            'content'   => $content,
+            'keyType'   => strtolower($keyType) === 'name' ? 'name' : 'http-equiv',
+            'scheme'    => $scheme,
+            'xhtml'     => $xhtml === true ? true : false
+        );
+    }
+
+    protected function setCustomHtmlHead($html)
+    {
+        $this->customHtmlHead = $html;
+    }
+}
